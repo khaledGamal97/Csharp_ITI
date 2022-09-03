fdc1908e651e6fc903831f0b710206e08912e7c0
khaledmeshaka97 <khaledmeshaka97@gmail.com>
2022-09-03 12:42:42 +0200

    03-09-2022 update

---
+++ Day8/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+namespace Day8
+{
+    class Program
+    {
+        static void Main1()
+        {
+
+            //create list of strings
+            List <string> list = new List<string>();
+
+            //add items in the list
+            list.Add("khaled");
+            list.Add("ahmed");
+            list.Add("ali");
+            list.Add("sara");
+
+            //print list's items
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===========================");
+
+            //find list's items which length > 3 
+            foreach(string item in list.FindAll((item) => item.Length > 3))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===========================");
+
+            //find list's items which starts with 'a'
+            foreach (string item in list.FindAll((item) => item.StartsWith('a')))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===========================");
+
+            //find list's items which ends with 'a'
+            foreach (string item in list.FindAll((item) => item.EndsWith('a')))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===========================");
+
+
+
+        }
+        static void Main()
+        {
+            //create list of students
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Id = 1, Name = "khaled", Age = 25 });
+            students.Add(new Student() { Id = 2, Name = "ahmed", Age = 24 });
+            students.Add(new Student() { Id = 3, Name = "john", Age = 18 });
+
+            //print list of students
+            foreach(Student student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.WriteLine("===========================");
+
+            //print students have age between 20 and 30
+            foreach(Student student in students.FindAll((student) => student.Age > 20 && student.Age < 30))
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.WriteLine("===========================");
+
+            //print students have name length > 5
+            foreach (Student student in students.FindAll((student) => student.Name.Length > 5))
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.WriteLine("===========================");
+
+            //print students have name contains 'a'
+            foreach (Student student in students.FindAll((student) => student.Name.Contains('a')))
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.WriteLine("===========================");
+        }
+    }
+}
---
+++ Day8/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Day8
+{
+    class Student
+    {
+        int id;
+        string name;
+        int age;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"id:{id} - name:{name} - age:{age}";
+        }
+    }
+}
